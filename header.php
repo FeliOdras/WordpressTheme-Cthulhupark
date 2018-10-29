e09de22c602a9736fd81413588fb34d427ce00bb
FeliOdras <ariane.feldmann@odras.de>
2018-10-29 11:37:13 +0100

    Basic header information and placeholders added

---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <div id="page">
+        <header id="main-header">
+        Include custom header image here<br />
+        Include main navigation here <br />
