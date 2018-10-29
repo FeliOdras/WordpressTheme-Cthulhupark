3851380b31cfa0a4b9616c0c60ae275cd37e72aa
FeliOdras <ariane.feldmann@odras.de>
2018-10-29 13:53:52 +0100

    Information for header.php added

--- header.php
+++ header.php
@@ -4,10 +4,7 @@
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
+ * @package Cthulhupark
  */
 ?>
 <!DOCTYPE html>
