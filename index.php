9411c48c962a7eb27f66389184da018f53c6a0a6
FeliOdras <ariane.feldmann@odras.de>
2018-10-30 09:49:09 +0100

    Content to index.php added

--- index.php
+++ index.php
@@ -14,7 +14,21 @@ get_header();
 ?>
     <div id="primary" class="content-area">
 		<main id="main" class="site-main">
-            <h1>Hello World</h1>
+        <?php 
+            if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+        ?>
+        
+        <h2><?php the_title() ?></h2>
+		<?php the_content() ?>
+
+        <?php
+            endwhile;
+            else :
+            echo '<p>Hier gibt es leider nichts zu sehen</p>';
+            endif;
+        ?>
+ 
         </main><!-- #main -->
     </div> <!-- #primary -->
 
