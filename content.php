4e6919140ee668c9e776f28853782c25bb61be1e
FeliOdras <ariane.feldmann@odras.de>
2018-10-30 15:59:26 +0100

    Content.php added

---
+++ content.php
@@ -0,0 +1,8 @@
+<div id="main-content">
+    <?php
+    if ( is_archive() ) || (is_category()) :
+    <h2 class="blog-post-title">Sample blog post</h2>
+        <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+            php the_content();
+    ?>
+</div>
