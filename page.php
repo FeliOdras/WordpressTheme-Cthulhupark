ccae417ca3c087cd050dab3162e847e59a3319dd
FeliOdras <ariane.feldmann@odras.de>
2018-11-01 09:56:37 +0100

    Content section added to page template

--- page.php
+++ page.php
@@ -10,7 +10,10 @@ get_header();
 
 <div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+          <?php while ( have_posts() ) : the_post(); ?>
+                <h2><?php  the_title(); ?></h2>
+                <?php the_content(); ?>
+            <?php endwhile; // end of the loop. ?>
         </main><!-- #main -->
     </div> <!-- #primary -->
 
