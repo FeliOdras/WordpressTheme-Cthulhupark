be59185de6806dc798097c0ed00d114f25ab8b20
FeliOdras <ariane.feldmann@odras.de>
2018-11-01 09:47:40 +0100

    Content Part added to single post template

--- single.php
+++ single.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * The single post template file
- *
- * This is to organize categories and archives of our blog.
+ * The single post template file	
  *
  * @package Cthulhupark 
  */
@@ -12,11 +10,12 @@ get_header();
 
 <div id="primary" class="content-area">
 		<main id="main" class="site-main">
-            <header class="page-header">
-                <h1><?php the_title(); ?></h1>
-            </header> <!-- .page-header-->
+            <?php while ( have_posts() ) : the_post(); ?>
+                <h3><?php the_category('&nbsp;&rsaquo;&nbsp;'); echo "&nbsp;&rsaquo;&nbsp;"; the_title(); ?></h3>
+                <?php the_content(); ?>
+            <?php endwhile; // end of the loop. ?>
         </main><!-- #main -->
     </div> <!-- #primary -->
-
+    
 <?php
 get_footer();
