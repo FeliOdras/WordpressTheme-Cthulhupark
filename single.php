39f6a0ef347000fce9d2e34a1d7f89e8fc45115d
FeliOdras <ariane.feldmann@odras.de>
2018-11-01 09:18:43 +0100

    Setup of basich structure in single post template exit quit

---
+++ single.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The single post template file
+ *
+ * This is to organize categories and archives of our blog.
+ *
+ * @package Cthulhupark 
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+            <header class="page-header">
+
+        </main><!-- #main -->
+    </div> <!-- #primary -->
+
+<?php
+get_footer();
