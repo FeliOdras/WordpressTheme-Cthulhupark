bc0e6f6fd013efa933d0ad79af091fa854f5f5d5
FeliOdras <ariane.feldmann@odras.de>
2018-11-01 09:50:18 +0100

    Setup of basic structure of page template

---
+++ page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The page template file	
+ *
+ * @package Cthulhupark 
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+        </main><!-- #main -->
+    </div> <!-- #primary -->
+
+<?php
+get_footer();
--- single.php
+++ single.php
@@ -8,7 +8,7 @@
 get_header();
 ?>
 
-<div id="primary" class="content-area">
+    <div id="primary" class="content-area">
 		<main id="main" class="site-main">
             <?php while ( have_posts() ) : the_post(); ?>
                 <h3><?php the_category('&nbsp;&rsaquo;&nbsp;'); echo "&nbsp;&rsaquo;&nbsp;"; the_title(); ?></h3>
@@ -16,6 +16,6 @@ get_header();
             <?php endwhile; // end of the loop. ?>
         </main><!-- #main -->
     </div> <!-- #primary -->
-    
+
 <?php
 get_footer();
