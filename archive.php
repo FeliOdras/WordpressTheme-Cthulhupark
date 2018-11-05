329998649bd5e536abb150ff5b28f9df82860afc
FeliOdras <ariane.feldmann@odras.de>
2018-11-05 13:36:01 +0100

    Removed category link on single post template

--- archive.php
+++ archive.php
@@ -21,7 +21,7 @@ get_header();
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <article class="post-main <?php if (is_category(abenteurertagebuch)) :?>dairy<?php elseif (is_category(traumwelten)) :?>dreamlands<?php endif;?>">
                     <h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" ><?php the_title(); ?></a></h2>
-                    <div class="post-meta"
+                    <div class="post-meta">
                     <div class="featured-image">
                         <?php 
                         // check if the post has a Post Thumbnail assigned to it.
--- single.php
+++ single.php
@@ -12,7 +12,7 @@ get_header();
 		<main id="main" class="site-main">
             <?php get_sidebar(left); ?>
                 <?php while ( have_posts() ) : the_post(); ?>
-                    <h3><?php the_category('&nbsp;&rsaquo;&nbsp;'); echo "&nbsp;&rsaquo;&nbsp;"; the_title(); ?></h3>
+                    <h3><?php the_title(); ?></h3>
                     <?php the_content(); ?>
                 <?php endwhile; // end of the loop. ?>
             <?php get_sidebar(right); ?>
