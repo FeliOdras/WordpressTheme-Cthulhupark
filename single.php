6a5138ff51fb3d14b99733577cccd8727e5f2ed1
FeliOdras <ariane.feldmann@odras.de>
2018-11-05 14:19:54 +0100

    Author added to single post template

--- single.php
+++ single.php
@@ -15,6 +15,7 @@ get_header();
                     <div class="main-post <?php if (in_category(traumwelten)) :?>dreamlands<?php elseif (in_category(abenteurertagebuch)) :?>dairy<?php endif;?>">
                         <h3><?php the_title(); ?></h3>
                         <?php the_content(); ?>
+                        <div class="author">Von <?php the_author(); ?></div>
                     </div><!-- .main-post -->
                     <div class="post-navigation">
                     <?php next_post_link('%link', '%title', TRUE); ?> ~ <?php edit_post_link('Bearbeiten', '', ''); ?> ~ <?php previous_post_link('%link', '%title', TRUE); ?>
