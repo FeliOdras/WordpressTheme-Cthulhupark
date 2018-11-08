f6cfa82d391fa12381d8bfc1005540d00dfd8dba
FeliOdras <ariane.feldmann@odras.de>
2018-11-08 11:18:59 +0100

    Title and subtitle styled

--- header.php
+++ header.php
@@ -19,8 +19,10 @@
 <body <?php body_class(); ?>>
     <div id="page">
         <header id="main-header">
-            <h1 class-"main-title"><a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
-            <h2 class="sub-title"><?php bloginfo( 'description' ); ?></h2>
+            <div id="blog-info">
+                <h1 class="main-title"><a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+                <h2 class="sub-title"><?php bloginfo( 'description' ); ?></h2>
+            </div>
             <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
             <nav id="main-navigation">
                 <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
