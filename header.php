7b460d528bab1cab7582c1ae0f0127cee1660d7f
FeliOdras <ariane.feldmann@odras.de>
2018-11-11 13:08:19 +0100

    Added CSS classes to main elements in header add. to ID

--- header.php
+++ header.php
@@ -18,13 +18,13 @@
 </head>
 <body <?php body_class(); ?>>
     <div id="page">
-        <header id="main-header">
-            <div id="blog-info">
+        <header id="main-header" class="main-header">
+            <div id="blog-info" class="blog-info">
                 <h1 class="main-title"><a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
                 <h2 class="sub-title"><?php bloginfo( 'description' ); ?></h2>
             </div>
             <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
-            <nav id="main-navigation">
+            <nav id="main-navigation" class="main-navigation">
                 <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
             </nav>
         </header>
