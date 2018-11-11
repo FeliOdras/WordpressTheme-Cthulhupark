03726a7520c4c383c6d065a46d3c2d96adb6b57b
FeliOdras <ariane.feldmann@odras.de>
2018-11-11 19:07:09 +0100

    CSS class sidebar added to asides

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,4 +1,4 @@
-<aside id="sidebar-left" class="sidebar-left">
+<aside id="sidebar-left" class="sidebar-left sidebar ">
 <?php if ( is_active_sidebar( 'sidebar-left' ) ) : 
     dynamic_sidebar( 'sidebar-left' ); 
 endif; ?>
--- sidebar-right.php
+++ sidebar-right.php
@@ -1,4 +1,4 @@
-<aside id="sidebar-right" class="sidebar-right">
+<aside id="sidebar-right" class="sidebar-right sidebar">
 <?php if ( is_active_sidebar( 'sidebar-right' ) ) : 
     dynamic_sidebar( 'sidebar-right' ); 
 endif; ?>
