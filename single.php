8913c703e95d993d4f7ef31f3288c1771bd85aea
FeliOdras <ariane.feldmann@odras.de>
2018-11-11 19:46:04 +0100

    Added section main-content to single post template

--- single.php
+++ single.php
@@ -12,17 +12,18 @@ get_header();
 		<main id="main" class="site-main">
             <?php get_sidebar(left); ?>
                 <?php while ( have_posts() ) : the_post(); ?>
+                   <section id="main-entry" class="main-entry">
                     <article class="main-post <?php if (in_category(traumwelten)) :?>dreamlands<?php elseif (in_category(abenteurertagebuch)) :?>dairy<?php endif;?> <?php the_author_nickname() ?>">
                         <time class="date"><?php the_time('l, j. F Y') ?></time>
                         <h3><?php the_title(); ?></h3>
                         <?php the_content(); ?>
                         <div class="author"><?php _e('By', 'cthulhupark') ?> <?php the_author_posts_link(); ?></div>
 
-                    <div class="post-navigation">
-                    <?php next_post_link('%link', '%title', TRUE); ?> ~ <?php edit_post_link('Edit', '', ''); ?> ~ <?php previous_post_link('%link', '%title', TRUE); ?>
-                    </div>
+                        <footer class="post-navigation">
+                            <?php next_post_link('%link', '%title', TRUE); ?> ~ <?php edit_post_link('Edit', '', ''); ?> ~ <?php previous_post_link('%link', '%title', TRUE); ?>
+                        </footer>
                     </article><!-- .main-post -->
-                    
+                    </section>         
                 <?php endwhile; // end of the loop. ?>
             <?php get_sidebar(right); ?>
         </main><!-- #main -->
