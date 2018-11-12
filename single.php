a0bfd928375dce9e10e32c0b969b342668371ef2
FeliOdras <ariane.feldmann@odras.de>
2018-11-12 15:48:45 +0100

    Added conditions to display date and post nav only on certain categrories

--- single.php
+++ single.php
@@ -14,16 +14,17 @@ get_header();
                 <?php while ( have_posts() ) : the_post(); ?>
                    <section id="main-entry" class="main-entry">
                     <article class="entry-content <?php if (in_category(traumwelten)) :?>dreamlands<?php elseif (in_category(abenteurertagebuch)) :?>dairy<?php endif;?> <?php the_author_nickname() ?>">
-                       
+                       <?php if (in_category(traumwelten) || in_category(abenteurertagebuch)) :?>
                             <time class="date"><?php the_time('l, j. F Y') ?></time>
-                
+                        <?php endif; ?>
                         <h2 class="entry-title"><?php the_title(); ?></h2>
                         <?php the_content(); ?>
                         <div class="author"><?php _e('By', 'cthulhupark') ?> <?php the_author_posts_link(); ?></div>
-
-                        <footer class="post-navigation">
-                            <?php next_post_link('%link', '%title', TRUE); ?> ~ <?php edit_post_link('Edit', '', ''); ?> ~ <?php previous_post_link('%link', '%title', TRUE); ?>
-                        </footer>
+                        <?php if (in_category(traumwelten) || in_category(abenteurertagebuch)) :?>
+                            <footer class="post-navigation">
+                                <?php next_post_link('%link', '%title', TRUE); ?> ~ <?php edit_post_link('Edit', '', ''); ?> ~ <?php previous_post_link('%link', '%title', TRUE); ?>
+                            </footer>
+                        <?php endif; ?>
                     </article><!-- .main-post -->
                     </section>         
                 <?php endwhile; // end of the loop. ?>
