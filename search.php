508da395e8f33021f0d27eb8ec9c8e28faa64f39
FeliOdras <ariane.feldmann@odras.de>
2018-11-22 15:31:51 +0100

    Created 404 error page, minor adjustments on search template

--- search.php
+++ search.php
@@ -1,8 +1,11 @@
 <?php
 
-/*
- Template Name: Tags
- Template Post Type: post, page
+/**
+ * The search results template file
+ *
+ * This is to organize categories and archives of our blog.
+ *
+ * @package Cthulhupark 
  */
 get_header();
 ?>
@@ -12,24 +15,25 @@ get_header();
         <?php get_sidebar('left'); ?>
         <section id="main-entry" class="main-entry">
             <article class="entry-content">
-            <h1 class="entry-title"><?php  the_title(); ?></h1>
-            <article><?php echo $wp_query->found_posts; ?> <?php _e( 'Suchergebnisse für ', 'locale' ); ?>: "<?php the_search_query(); ?>"</article>
-            <?php
-			// Start the Loop.
-            while ( have_posts() ) : the_post() ?>
-             <div class="archive-entry">
-                 <header class="post-header">                                   
-                    <h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" ><?php the_title(); ?></a></h2>
-                    <div class="post-excerpt">
-                        <?php the_excerpt () ?>
-                    </div> <!-- .post-excerpt -->
-                    <footer class="search-meta">
-                        Kategorien: <?php echo get_the_category_list(); ?><br />
-                        <?php the_tags(); ?>
-                    </footer>
-                </header>
-			</div>	
-			<?php endwhile; ?>
+                <h1 class="entry-title"><?php  the_title(); ?></h1>
+                <article><?php echo $wp_query->found_posts; ?> <?php _e( 'Suchergebnisse für ', 'locale' ); ?>: "<?php the_search_query(); ?>"</article>
+                <?php
+			    // Start the Loop.
+                while ( have_posts() ) : the_post() ?>
+                    <div class="archive-entry">
+                        <header class="post-header">                                   
+                            <h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" ><?php the_title(); ?></a></h2>
+                        </header>
+                        <div class="post-excerpt">
+                            <?php the_excerpt () ?>
+                        </div> <!-- .post-excerpt -->
+                        <footer class="search-meta">
+                            Kategorien: <?php echo get_the_category_list(); ?><br />
+                            <?php the_tags(); ?>
+                        </footer>              
+			        </div>	
+                <?php endwhile; ?>
+            </article>
         </section>
         <?php 
             get_sidebar('right'); 
