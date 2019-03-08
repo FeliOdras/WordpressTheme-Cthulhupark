33bc194bbc41ce9510bd221310e35a8d260f828c
odras <ariane.feldmann@odras.de>
2019-03-08 18:53:07 +0100

    Integrated wp rest api for post output

--- js/cthulhupark-main.js
+++ js/cthulhupark-main.js
@@ -1,7 +1,17 @@
-console.log("HELLO WORLD")
-
 responsiveMenu = () => {
     const menuContainer = document.querySelector("#navigation-container");
     menuContainer.classList.toggle("visible");
 }
 
+getWPApi = () => {
+    fetch('https://hq.cthulhupark.de/wp-json/wp/v2/posts/?categories=2&&per_page=50&&orderby=modified')
+    .then(response => response.json())
+    .then (data =>{ 
+        console.log(data)
+        data.forEach(posts => {
+            console.log(`Title ${posts.title.rendered}. Last modified ${posts.modified}`)
+        });
+    })
+    .catch (error => console.log('something went wrong'))
+}
+(getWPApi())
