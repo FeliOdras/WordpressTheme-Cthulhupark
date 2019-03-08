1663c7d78686ceaa0a256207a50b6c4a632f0e47
odras <ariane.feldmann@odras.de>
2019-03-08 23:47:08 +0100

    Created JS class for lastestTBE

--- js/cthulhupark-main.js
+++ js/cthulhupark-main.js
@@ -7,7 +7,8 @@ class LatestDairies {
     constructor(domselector) {
         this.apiUrl='https://hq.cthulhupark.de/wp-json/wp/v2/posts/?categories=2&&per_page=50&&orderby=modified';
         this.htmlContainer = document.querySelector(domselector);
-        this.fetchData()
+        this.entriesOutput = ``;
+        this.fetchData();
     }
 
     fetchData() {
@@ -19,32 +20,41 @@ class LatestDairies {
         })
     }
 
+    addDiffDays() {
+        let tbeEntries = this.tbeEntries;
+        tbeEntries.forEach (entry => {
+            let modDate = entry.modified;
+            let nowDate = new Date();
+            let timeDiff = Math.abs(Date.parse(modDate) - nowDate.getTime());
+            let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            entry.diffDays = diffDays;      
+        });
+        return tbeEntries;
+    }
+
+    showLatest(){
+        let entryList = this.addDiffDays();
+        let latestEntries = entryList.filter(entry => entry.diffDays < 21);
+        console.log(latestEntries)
+        return latestEntries;
+    }
+
     template(){
-        let entryList = this.tbeEntries;
-        console.log(entryList);
-        return entryList;
+        let entryList = this.showLatest();
+        return entryList
+        .map (entry => {
+            return`
+                <a href="${entry.link}" title="${entry.title.rendered}">
+                    ${entry.title.rendered}</a><br>
+                <small>(vor ${entry.diffDays} ${entry.diffDays == 1 ? 'Tag' : 'Tagen'})</small><br>
+            `
+        }).join('')
     }
 
     render(){
-        console.log(this.tbeEntries)
-        let template = this.template()
-        let output = ``;
-        output += template;
+        let output = this.template();
         this.htmlContainer.innerHTML = output;
     }
 
 }
 const latestTBE = new LatestDairies('.latestTBE')
-
-// getLatestsDairies = () => {
-//     fetch('https://hq.cthulhupark.de/wp-json/wp/v2/posts/?categories=2&&per_page=50&&orderby=modified')
-//     .then(response => response.json())
-//     .then (dairyEntries =>{ 
-//         console.log(dairyEntries)
-//         dairyEntries.forEach(posts => {
-//             console.log(`Title ${posts.title.rendered}. Last modified ${posts.modified}`)
-//         });
-//     })
-//     .catch (error => console.log('something went wrong'))
-// }
-//(getLatestsDairies())
