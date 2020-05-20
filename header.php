26fb662a74f6c5dd988ad15e1af83b5b23a9f7ce
odras <ariane.feldmann@odras.de>
2020-05-20 17:51:01 +0200

    Fixed header image position

--- header.php
+++ header.php
@@ -18,13 +18,13 @@
 </head>
 <body <?php body_class(); ?>>
     <div id="page">
-        <header id="main-header" class="main-header">
+        <header id="main-header" class="main-header" style="background-image:url('<?php header_image(); ?>')">
             <div class="main-header-wrapper">
                 <div id="blog-info" class="blog-info">
                     <h1 class="main-title"><a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
                     <h2 class="sub-title"><?php bloginfo( 'description' ); ?></h2>
                 </div>
-                <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+                <!--<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />-->
             </div>
             <nav id="main-navigation" class="main-navigation">
                 <button class="nav-open" onclick="responsiveMenu()"><i></i><i></i><i></i></button>
